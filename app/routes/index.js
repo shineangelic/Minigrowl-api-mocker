fbc1f9712e4fa89c58703b4708068b3156c8f58e
Hatem Alimam <me@hatemalimam.com>
2020-04-18 19:47:55 +0300

    fix linter issues

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+// eslint-disable-next-line no-unused-vars
+router.get('/', (req, res, next) => {
   res.json({
-    message: 'Welcome to Minigrowl api mocker',
+    message: 'Welcome to Minigrowl api mocker'
   });
 });
 
--- app/routes/sensors/router.js
+++ app/routes/sensors/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
+
 const router = express.Router();
 
 const response = require('./response');
 
-/* GET home page. */
-router.get('/sensors', function(req, res, next) {
+// eslint-disable-next-line no-unused-vars
+router.get('/sensors', (req, res, next) => {
   res.json(response);
 });
 
